39e46b1540227cfadf2b3932fb4ba248fe9ac667
Salvador Sánchez <salsanzmelc@gmail.com>
2023-06-20 18:17:02 -0600

    Iniciando a trabajar el SEO 10

--- layouts/BasicLayout/BasicLayout.js
+++ layouts/BasicLayout/BasicLayout.js
@@ -10,28 +10,30 @@ export default function BasicLayout({ children, title, project, platform, pageDe
   const regex = /(<([^>]+)>)/ig; // Expresión regular para encontrar todas las etiquetas HTML
   const description = project ? pageDescription.replace(regex, '') : '' // Remover todas las etiquetas HTML    
 
+  const actualImageUrl = `https://www.byodisy.com${imageUrl}`;
   return (
     <>
       <Head>
         <title>{titleActual}</title>
 
+        <meta name='og:title' content={titleActual} />
         <meta property="og:url" content={currentUrl} />
-
+        <meta property="og:type" content="website" />
         <meta name='description' content={description} />
         <meta name='og:description' content={description} />
-        <meta name='og:title' content={titleActual} />
-        <meta property="og:image:type" content="image/png" />
         {
           imageUrl && (
-            <meta name='og:image' content={imageUrl} />
+            <meta name='og:image' content={actualImageUrl} />
           )
         }
+        <meta property="og:image:type" content="image/png" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
 
       </Head>
 
       <div>
+
         <Header />
       </div>
       {children}
--- pages/_document.js
+++ pages/_document.js
@@ -6,11 +6,11 @@ export default function Document() {
       <Head>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
 
-        {/* Etiquetas para Facebook */}
         <meta name="google-site-verification" content="JzSQH_lxAd9eEJDh_-lDjH7dFsvhf-gSC1yvGrQ9v9g" />
+
         <meta property="og:url" content="https://www.byodisy.com" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="/public/imageHome.jpg" />
+        <meta property="og:image" content="/imageHome.jpg" />
         <meta property="og:image:type" content="image/jpg" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
