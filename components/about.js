ee0fe2f0e14c76bbbd5dde999706372cd1ff3778
Salvador Sánchez <salsanzmelc@gmail.com>
2023-06-24 10:48:44 -0600

    Modificando descripción

--- components/about.js
+++ components/about.js
@@ -8,9 +8,10 @@ export default function About() {
         <Image className="about__img" height='900' src={usImg} alt='Branding image' />
         <div className="about__text" id='ind__three'>
           <h2 className="about__title"><span>03 </span>Nosotros</h2>
-          <p>Somos un estudio de diseño y desarrollo de sitios y aplicaciones web.
-            <br /><br /> Nuestro objetivo es ofrecer soluciones a problemas de comunicación, mediante el diseño digital y la informática, utilizando para esto herramientas de software destinadas a la web.
-            <br /><br />Asesoramos a profesionistas y a pequeñas y medianas empresas.
+          <p>Somos un estudio de diseño y desarrollo web ubicado en Morelia.
+
+            <br /><br /> Ayudamos a pequeñas y medianas empresas a dar a conocer sus productos o servicios en internet.
+            <br /><br /> Creamos soluciones sencillas con tecnología y diseño.
           </p>
           <div className="decora__text"></div>
         </div>
